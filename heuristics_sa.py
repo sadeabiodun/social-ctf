28a61a4286338af4eaa802cab268e8fd1e1f40ca
Sade Abiodun <fabiodun@princeton.edu>
2021-05-31 19:12:04 -0400

    spawncamping video generation

--- heuristics_sa.py
+++ heuristics_sa.py
@@ -8,7 +8,7 @@ from features import get_features
 
 #import luke's heuristics functions 
 from ctf_dataset.behaviours.heuristic import near_teammate
-from ctf_dataset.behaviours.heuristic import local_behaviour_length, pad_behaviour,  behvaiour_start_and_end, camp_own_base, camp_opponent_base, running_forwards, running_backwards, approaching_own_base, approaching_opponent_base, approaching_own_flag, approaching_opponent_flag, approaching_teammate
+from ctf_dataset.behaviours.heuristic import camp_own_base, camp_opponent_base, running_forwards, running_backwards, approaching_own_base, approaching_opponent_base, approaching_own_flag, approaching_opponent_flag, approaching_teammate
 
 
 #import sam's heuristics functions 
@@ -29,18 +29,88 @@ player_id = slice(None) # 0-3
 # Test functions by viewing output for one matchup one repeat 
 
 #testing sam's following function 
-test_sam = get_following(wrap_f, matchup_id=0, repeat_id=0)
+following_sam = get_following(wrap_f, matchup_id=0, repeat_id=0)
 
 #testing luke's following function 
-test_luke = near_teammate(wrap_f, matchup_id=0, repeat_id=0)
+following_luke = near_teammate(wrap_f, map_id=0, matchup_id=0, repeat_id=0,
+                         min_behaviour_length=30, teammate_radius=2)
 
 ## Plotting to verify 
 
-plt.plot(test_sam[0].T, label='sam p1')
-plt.plot(test_sam[1].T, label='sam p2')
+plt.plot(following_sam[0].T, label='sam p1')
+plt.plot(following_sam[1].T, label='sam p2')
 
-plt.plot(p1[:,0].T, label='luke p1')
-plt.plot(p2[:,0].T, label='luke p2')
+# Plotting Luke's 
+fig, axs = plt.subplots(6,1)
+axs[0].plot(following_luke[0,:,0])
+axs[1].plot(following_luke[1,:,0])
+axs[2].plot(following_luke[2,:,0])
+axs[3].plot(following_luke[3,:,0])
+axs[4].plot(following_sam[0,:])
+axs[5].plot(following_sam[1,:])
+
+# Compare Sam and Luke's directly 
+# It appears that Luke's function takes account of isalive and this limits the time courses for following (as it should be)
+
+fig, axs = plt.subplots(2,1)
+axs[0].plot(following_luke[0,:500,0], label='luke')
+axs[1].plot(following_sam[0,:500], label='sam')
 plt.legend()
 
-## current conundrum, the output for luke and sam's functions do not match, even when setting matchup and repeat id to 0 
+from animations import time_series_animation
+from IPython.display import Video
+
+## Testing basecamping functions 
+
+camp_own = camp_own_base(wrap_f, map_id=0, matchup_id=0, repeat_id=0,
+                         min_behaviour_length=15, base_radius=1)
+
+# Plot 
+
+fig, axs = plt.subplots(4,1)
+axs[0].plot(camp_own[0,:,0], label='p1')
+axs[1].plot(camp_own[1,:,0], label='p2')
+axs[2].plot(camp_own[2,:,0], label='p3')
+axs[3].plot(camp_own[3,:,0], label='p4')
+fig.legend()
+
+## Animation 
+map_id = 0
+matchup_id = 0
+repeat_id = 0
+anim = time_series_animation(camp_own[[0,2],:,0], wrap_f, map_id=map_id, matchup_id=matchup_id, repeat_id=repeat_id, label='basecamping')
+
+
+anim.save(f'figures/time_series_animation_basecamping_min-br_m{matchup_id}_'
+          f'r{repeat_id}.mp4', dpi=90)
+
+Video(f'figures/time_series_animation_basecamping_min-br_m{matchup_id}_' f'r{repeat_id}.mp4')
+
+## Testing basecamping functions 
+
+camp_opp = camp_opponent_base(wrap_f, map_id=0, matchup_id=0, repeat_id=0,
+                         min_behaviour_length=15, base_radius=1)
+
+# Plot 
+
+fig, axs = plt.subplots(4,1)
+axs[0].plot(camp_opp[0,:,0], label='p1')
+axs[1].plot(camp_opp[1,:,0], label='p2')
+axs[2].plot(camp_opp[2,:,0], label='p3')
+axs[3].plot(camp_opp[3,:,0], label='p4')
+fig.legend()
+
+## Animation 
+map_id = 0
+matchup_id = 0
+repeat_id = 0
+anim = time_series_animation(camp_opp[[0,2],:,0], wrap_f, map_id=map_id, matchup_id=matchup_id, repeat_id=repeat_id, label='spawncamping')
+
+
+anim.save(f'figures/time_series_animation_spawncamping_min-br_m{matchup_id}_' f'r{repeat_id}.mp4', dpi=90)
+
+Video(f'figures/time_series_animation_spawncamping_min-br_m{matchup_id}_' f'r{repeat_id}.mp4')
+
+### It appears that what we're categorizing as 'spawncamping' might actually be being 'trapped' at opponent's plate after flag capture but before being killed. A kill follows almost every instance of 'spawncamping'
+
+
